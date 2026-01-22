c2a521ba8a5b92b6b0d8d0dad61c99559070f630
Yurii <pamkivurij92@gmail.com>
2026-01-22 14:47:16 +0100

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,7 @@ from app.main import get_human_age
         pytest.param(24, 24, [2, 2], id="24 - 24 - [2, 2]"),
         pytest.param(27, 27, [2, 2], id="27 - 27 - [2, 2]"),
         pytest.param(28, 28, [3, 2], id="28 - 28 - [3, 2]"),
+        pytest.param(29, 29, [3, 3], id="29 - 29 - [3, 3]"),
         pytest.param(100, 100, [21, 17], id="100 - 100 - [21, 17]"),
         pytest.param(-1, -1, [0, 0], id="-1 - -1 - [0, 0]"),
     ]
@@ -27,17 +28,19 @@ def test_should_return_correct_values(
 
 
 @pytest.mark.parametrize(
-    "cat_age,exception",
+    "cat_age,dog_age,exception",
     [
-        pytest.param("0", TypeError, id="str - TypeError"),
-        pytest.param([0, 1], TypeError, id="list - TypeError"),
-        pytest.param((0, 0), TypeError, id="tuple - TypeError"),
-        pytest.param({0: 0, 1: 1}, TypeError, id="dict - TypeError"),
+        pytest.param("0", 0, TypeError, id="str - int - TypeError"),
+        pytest.param([0, 1], 0, TypeError, id="list - int - TypeError"),
+        pytest.param((0, 0), 1, TypeError, id="tuple - int - TypeError"),
+        pytest.param({0: 0, 1: 1}, 0, TypeError, id="dict - int - TypeError"),
+        pytest.param(1, None, TypeError, id="int - None - TypeError"),
     ]
 )
 def test_should_raise_correct_error(
         cat_age: Any,
+        dog_age: Any,
         exception: Any
 ) -> None:
     with pytest.raises(exception):
-        get_human_age(cat_age, 0)
+        get_human_age(cat_age, dog_age)
